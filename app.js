6e3f0c4591e206dd75b301a5645e04500433dd68
Yu-Jin Tan <y.tan@trafixiq.com>
2020-05-18 15:55:57 +0100

    serving a basic html page

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const app = express();
 const morgan = require('morgan');
+const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.use(morgan('tiny')); //tiny - less GET info 
+app.use(morgan('tiny')); //tiny - less GET info
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
 app.listen(port, () => { //debug doesnt come up on production
     debug(`running on port ${chalk.green(port)}`); //using debug instead of console.log
 });
