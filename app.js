ff09b31c7711231ae105aa20a0161749e24e9c41
Yu-Jin Tan <y.tan@trafixiq.com>
2020-05-21 10:53:52 +0100

    seperating the routes to another file

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const morgan = require('morgan');
 const path = require('path');
 const port = process.env.PORT || 3000;
-const movieRouter = express.Router();
+
 
 app.use(morgan('tiny')); //tiny - less GET info
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -15,72 +15,7 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')
 app.set('views','./src/views');
 app.set('view engine', 'ejs');
 
-const movies = [
-    {
-        title: 'War Of The Worlds',
-        genre: 'Historical Fiction',
-        director: 'Steven Spielberg',
-        watched: false
-    },
-    {
-        title: 'Les Misérables',
-        genre: 'Historical Fiction',
-        director: 'Tom Hooper',
-        watched: false
-    },
-    {
-        title: 'The Time Machine',
-        genre: 'Science Fiction',
-        director: 'Simon Wells',
-        watched: false
-    },
-    {
-        title: 'A Journey into the Center of the Earth',
-        genre: 'Science Fiction',
-        director: 'Eric Brevig',
-        watched: false
-    },
-    {
-        title: 'The Dark World',
-        genre: 'Fantasy',
-        director: 'Henry Kuttner',
-        watched: false
-    },
-    {
-        title: 'The Wind in the Willows',
-        genre: 'Fantasy',
-        director: 'Dave Ybwub',
-        watched: false
-    },
-    {
-        title: 'Triangle',
-        genre: 'History',
-        director: 'Christoper Smith',
-        watched: false
-    },
-    {
-        title: 'Ready Player One',
-        genre: 'Biography',
-        director: 'Steven Spielberg',
-        watched: false
-    }
-    ]
-
-movieRouter.route('/', )
-    .get((req, res) => {
-        res.render('movies', 
-        { 
-            nav: [{link: '/movies', title: 'Movies'}, {link: '/directors', title: 'Directors'}], 
-            title: "Library",
-            movies //movies: movies, shorthand
-        });
-    });
-
-movieRouter.route('single')
-    .get((req, res) => {
-        res.send('hello single movie')
-    });
-
+const movieRouter = require('./src/routes/movieRoutes');
 app.use('/movies', movieRouter);
 
 app.get('/', (req, res) => {
